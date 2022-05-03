e4df8ac492a53a41b98086f1616b5825cdd830d2
Manuel Guth <manuel.guth@cern.ch>
2022-05-03 14:53:45 +0200

    adding base functions

---
+++ puma/tests/test_plot_base.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Unit test script for the functions in base.py
+"""
+
+import unittest
+
+from puma.utils import logger, set_log_level
+from puma.base import PlotObject
+
+set_log_level(logger, "DEBUG")
+
+
+class plot_object_TestCase(unittest.TestCase):
+    """Test class for the puma.base PlotObject dataclass."""
+
+    def test_only_one_input_figsize(self):
+        with self.assertRaises(ValueError):
+            PlotObject(figsize=1)
+
+    def test_only_tuple_three_inputs_figsize(self):
+        with self.assertRaises(ValueError):
+            PlotObject(figsize=(1, 2, 3))
+
+    def test_tuple_input_figsize(self):
+        figsize = PlotObject(figsize=(1, 2)).figsize
+        self.assertEqual(figsize, (1, 2))
+
+    def test_list_input_figsize(self):
+        figsize = PlotObject(figsize=[1, 2]).figsize
+        self.assertEqual(figsize, (1, 2))
+
+    def test_list_input_wrong_len_figsize(self):
+        with self.assertRaises(ValueError):
+            PlotObject(figsize=[1, 2, 3])
+
+    def test_wrong_n_ratio_panels(self):
+        with self.assertRaises(ValueError):
+            PlotObject(n_ratio_panels=5)
