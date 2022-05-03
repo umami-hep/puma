e737b9841ed05e78c9cff4edd50954daef6071e6
Manuel Guth <manuel.guth@cern.ch>
2022-05-03 14:38:07 +0200

    adding utils

--- puma/__init__.py
+++ puma/__init__.py
@@ -1,2 +1,13 @@
 """puma framework - Plotting UMami Api."""
 __version__ = "0.0"
+
+
+def get_good_colours():
+    """List of colours adequate for plotting
+
+    Returns
+    -------
+    list
+        list with colours
+    """
+    return ["#AA3377", "#228833", "#4477AA", "#CCBB44", "#EE6677", "#BBBBBB"]
---
+++ puma/utils/__init__.py
@@ -0,0 +1,1 @@
+"""Module for usefule tools in puma."""
