ebe4716cd40ed62af7a2cd3a4c9d88995066fd72
Joschka Birk <joschka.birk@gmail.com>
2022-05-10 13:15:04 +0200

    Moving tests

--- puma/tests/test_plot_base.py
+++ puma/tests/test_plot_base.py
@@ -1,38 +1,40 @@
+#!/usr/bin/env python
+
 """
-Unit test script for the functions in base.py
+Unit test script for the functions in metrics.py
 """
 
 import unittest
 
-from puma.utils import logger, set_log_level
-from puma.base import PlotObject
+from puma.utils.logging import logger
+from puma import plot_object
 
-set_log_level(logger, "DEBUG")
+# set_log_level(logger, "DEBUG")
 
 
 class plot_object_TestCase(unittest.TestCase):
-    """Test class for the puma.base PlotObject dataclass."""
+    """Test class for the umami.plotting plot_object dataclass."""
 
     def test_only_one_input_figsize(self):
         with self.assertRaises(ValueError):
-            PlotObject(figsize=1)
+            plot_object(figsize=1)
 
     def test_only_tuple_three_inputs_figsize(self):
         with self.assertRaises(ValueError):
-            PlotObject(figsize=(1, 2, 3))
+            plot_object(figsize=(1, 2, 3))
 
     def test_tuple_input_figsize(self):
-        figsize = PlotObject(figsize=(1, 2)).figsize
+        figsize = plot_object(figsize=(1, 2)).figsize
         self.assertEqual(figsize, (1, 2))
 
     def test_list_input_figsize(self):
-        figsize = PlotObject(figsize=[1, 2]).figsize
+        figsize = plot_object(figsize=[1, 2]).figsize
         self.assertEqual(figsize, (1, 2))
 
     def test_list_input_wrong_len_figsize(self):
         with self.assertRaises(ValueError):
-            PlotObject(figsize=[1, 2, 3])
+            plot_object(figsize=[1, 2, 3])
 
     def test_wrong_n_ratio_panels(self):
         with self.assertRaises(ValueError):
-            PlotObject(n_ratio_panels=5)
+            plot_object(n_ratio_panels=5)
