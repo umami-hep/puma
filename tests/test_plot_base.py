4d8628386fb36a82e3d8c97018d1110f7c1621d6
Joschka Birk <joschka.birk@gmail.com>
2022-05-10 13:11:46 +0200

    Adding unit tests

---
+++ tests/test_plot_base.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Unit test script for the functions in metrics.py
+"""
+
+import unittest
+
+from puma.utils.logging import logger
+from puma import plot_object
+
+# set_log_level(logger, "DEBUG")
+
+
+class plot_object_TestCase(unittest.TestCase):
+    """Test class for the umami.plotting plot_object dataclass."""
+
+    def test_only_one_input_figsize(self):
+        with self.assertRaises(ValueError):
+            plot_object(figsize=1)
+
+    def test_only_tuple_three_inputs_figsize(self):
+        with self.assertRaises(ValueError):
+            plot_object(figsize=(1, 2, 3))
+
+    def test_tuple_input_figsize(self):
+        figsize = plot_object(figsize=(1, 2)).figsize
+        self.assertEqual(figsize, (1, 2))
+
+    def test_list_input_figsize(self):
+        figsize = plot_object(figsize=[1, 2]).figsize
+        self.assertEqual(figsize, (1, 2))
+
+    def test_list_input_wrong_len_figsize(self):
+        with self.assertRaises(ValueError):
+            plot_object(figsize=[1, 2, 3])
+
+    def test_wrong_n_ratio_panels(self):
+        with self.assertRaises(ValueError):
+            plot_object(n_ratio_panels=5)
---
+++ tests/test_roc.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+
+"""
+Unit test script for the functions in roc.py
+"""
+
+import os
+import tempfile
+import unittest
+
+import numpy as np
+from matplotlib.testing.compare import compare_images
+
+from puma.utils.logging import logger
+from puma import roc, roc_plot
+
+# set_log_level(logger, "DEBUG")
+
+
+class roc_TestCase(unittest.TestCase):
+    """Test class for the umami.plotting.roc functions."""
+
+    def setUp(self):
+        self.sig_eff = np.linspace(0.4, 1, 100)
+        self.bkg_rej = np.exp(-self.sig_eff) * 10e3
+
+    def test_roc_init(self):
+        """Test roc init."""
+        with self.assertRaises(ValueError):
+            roc(np.ones(4), np.ones(5))
+
+    def test_ratio_same_object(self):
+        """Test roc divide function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        roc_curve_ref = roc(self.sig_eff, self.bkg_rej)
+        _, ratio, _ = roc_curve.divide(roc_curve_ref)
+
+        np.testing.assert_array_almost_equal(ratio, np.ones(len(self.bkg_rej)))
+
+    def test_ratio_factor_two(self):
+        """Test roc divide function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        roc_curve_ref = roc(self.sig_eff, self.bkg_rej * 2)
+        _, ratio, _ = roc_curve.divide(roc_curve_ref)
+
+        np.testing.assert_array_almost_equal(ratio, 1 / 2 * np.ones(len(self.bkg_rej)))
+
+    def test_ratio_different_sig_interval(self):
+        """Test roc divide function."""
+        sig_eff = np.linspace(0.4, 0.9, 6)
+        sig_eff_ref = np.linspace(0.6, 1, 5)
+        bkg_rej = np.exp(-sig_eff) * 10e3
+        bkg_rej_ref = np.exp(-sig_eff_ref) * 10e3
+        roc_curve = roc(sig_eff, bkg_rej)
+        roc_curve_ref = roc(sig_eff_ref, bkg_rej_ref * 2)
+        sig_eff, ratio, _ = roc_curve.divide(roc_curve_ref)
+
+        np.testing.assert_array_almost_equal(
+            [sig_eff, ratio],
+            [np.linspace(0.6, 0.9, 4), 1 / 2 * np.ones(4)],
+        )
+
+    def test_ratio_factor_two_inverse(self):
+        """Test roc divide function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        roc_curve_ref = roc(self.sig_eff, self.bkg_rej * 2)
+        _, ratio, _ = roc_curve.divide(roc_curve_ref, inverse=True)
+
+        np.testing.assert_array_almost_equal(ratio, 2 * np.ones(len(self.bkg_rej)))
+
+    def test_binomial_error_no_ntest(self):
+        """Test roc binomial_error function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        with self.assertRaises(ValueError):
+            roc_curve.binomial_error()
+
+    def test_binomial_error_only_zeros(self):
+        """Test roc binomial_error function."""
+        roc_curve = roc(self.sig_eff, np.zeros(len(self.sig_eff)), n_test=10e5)
+        np.testing.assert_array_almost_equal(roc_curve.binomial_error(), [])
+
+    def test_binomial_error_example(self):
+        """Test roc binomial_error function."""
+        error_rej = np.array([8.717798, 35.0, 99.498744])
+        roc_curve = roc(np.array([0.1, 0.2, 0.3]), np.array([20, 50, 100]), n_test=100)
+        np.testing.assert_array_almost_equal(roc_curve.binomial_error(), error_rej)
+
+    def test_binomial_error_example_norm(self):
+        """Test roc binomial_error function."""
+        error_rej = np.array([8.717798, 35.0, 99.498744]) / np.array([20, 50, 100])
+        roc_curve = roc(np.array([0.1, 0.2, 0.3]), np.array([20, 50, 100]), n_test=100)
+        np.testing.assert_array_almost_equal(
+            roc_curve.binomial_error(norm=True), error_rej
+        )
+
+    def test_binomial_error_example_pass_ntest(self):
+        """Test roc binomial_error function."""
+        error_rej = np.array([8.717798, 35.0, 99.498744])
+        roc_curve = roc(np.array([0.1, 0.2, 0.3]), np.array([20, 50, 100]))
+        np.testing.assert_array_almost_equal(
+            roc_curve.binomial_error(n_test=100), error_rej
+        )
+
+    def test_fct_inter(self):
+        """Test roc fct_inter function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        np.testing.assert_array_almost_equal(
+            roc_curve.fct_inter(self.sig_eff), self.bkg_rej
+        )
+
+
+class roc_mask_TestCase(unittest.TestCase):
+    """Test class for the umami.plotting.roc non_zero_mask function."""
+
+    def setUp(self):
+        self.sig_eff = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7])
+        self.bkg_rej = np.array([0, 0.2, 0, 0.4, 0.5, 0, 0.7])
+
+    def test_non_zero_mask(self):
+        """Test roc non_zero_mask function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero_mask, [False, True, False, True, True, False, True]
+        )
+
+    def test_non_zero_mask_xmin(self):
+        """Test roc non_zero_mask function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej, xmin=0.4)
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero_mask, [False, False, False, True, True, False, True]
+        )
+
+    def test_non_zero_mask_xmax(self):
+        """Test roc non_zero_mask function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej, xmax=0.6)
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero_mask, [False, True, False, True, True, False, False]
+        )
+
+    def test_non_zero_mask_xmin_xmax(self):
+        """Test roc non_zero_mask function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej, xmax=0.6, xmin=0.4)
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero_mask, [False, False, False, True, True, False, False]
+        )
+
+    def test_non_zero(self):
+        """Test roc non_zero function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej)
+        result_bkg_rej = self.bkg_rej[[False, True, False, True, True, False, True]]
+        result_sig_eff = self.sig_eff[[False, True, False, True, True, False, True]]
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero, (result_bkg_rej, result_sig_eff)
+        )
+
+    def test_non_zero_xmin(self):
+        """Test roc non_zero function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej, xmin=0.4)
+        result_bkg_rej = self.bkg_rej[[False, False, False, True, True, False, True]]
+        result_sig_eff = self.sig_eff[[False, False, False, True, True, False, True]]
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero, (result_bkg_rej, result_sig_eff)
+        )
+
+    def test_non_zero_xmax(self):
+        """Test roc non_zero function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej, xmax=0.6)
+        result_bkg_rej = self.bkg_rej[[False, True, False, True, True, False, False]]
+        result_sig_eff = self.sig_eff[[False, True, False, True, True, False, False]]
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero, (result_bkg_rej, result_sig_eff)
+        )
+
+    def test_non_zero_xmin_xmax(self):
+        """Test roc non_zero function."""
+        roc_curve = roc(self.sig_eff, self.bkg_rej, xmax=0.6, xmin=0.4)
+        result_bkg_rej = self.bkg_rej[[False, False, False, True, True, False, False]]
+        result_sig_eff = self.sig_eff[[False, False, False, True, True, False, False]]
+        np.testing.assert_array_almost_equal(
+            roc_curve.non_zero, (result_bkg_rej, result_sig_eff)
+        )
+
+
+class roc_output_TestCase(unittest.TestCase):
+    """Test class for the umami.plotting.roc_plot function."""
+
+    def setUp(self):
+        # Set up temp directory for comparison plots
+        self.tmp_dir = tempfile.TemporaryDirectory()
+        self.actual_plots_dir = f"{self.tmp_dir.name}/"
+        self.expected_plots_dir = os.path.join(
+            os.path.dirname(__file__), "expected_plots"
+        )
+
+        self.sig_eff = np.array(
+            [0.5, 0.56, 0.63, 0.76, 0.83, 0.84, 0.85, 0.88, 0.9, 0.93, 1]
+        )
+        self.u_rej_1 = np.array([1542, 918, 426, 78, 31, 26, 22, 12, 8, 4, 1])
+        self.u_rej_2 = np.array([3061, 1642, 709, 112, 41, 34, 28, 15, 9, 4, 1])
+        self.c_rej_1 = np.array([26, 17, 10, 4.1, 2.9, 2.7, 2.6, 2.2, 1.9, 1.6, 1])
+        self.c_rej_2 = np.array([45, 27, 14, 4, 3, 2.9, 2.7, 2.2, 2.0, 1.63, 1])
+
+        # sig efficiency with different start
+        self.sig_eff_short = np.linspace(0.5, 1, 100)
+
+    def test_output_two_curves_one_ratio(self):
+        """Test with two curves of same flavour, one ratio panel"""
+        plot = roc_plot(
+            n_ratio_panels=1,
+            ylabel="background rejection",
+            xlabel="$b$-jets efficiency",
+            atlas_second_tag=(
+                "$\\sqrt{s}=13$ TeV, PFlow Jets,\n"
+                "$t\\bar{t}$ Test Sample, $f_{c}=0.018$"
+            ),
+            # logy=False,
+        )
+
+        # Add two roc curves
+        plot.add_roc(
+            roc(
+                self.sig_eff,
+                self.u_rej_1,
+                rej_class="ujets",
+                label="reference curve",
+            ),
+            reference=True,
+        )
+        plot.add_roc(
+            roc(
+                self.sig_eff,
+                self.u_rej_2,
+                rej_class="ujets",
+                label="second curve",
+            )
+        )
+
+        plot.set_ratio_class(1, "ujets", label="light-flavour jets ratio")
+
+        # Draw the figure
+        plot.draw()
+
+        plotname = "test_roc_two_curves_1_ratio.png"
+        plot.savefig(f"{self.actual_plots_dir}/{plotname}")
+        # Uncomment line below to update expected image
+        # plot.savefig(f"{self.expected_plots_dir}/{plotname}")
+        self.assertIsNone(
+            compare_images(
+                f"{self.actual_plots_dir}/{plotname}",
+                f"{self.expected_plots_dir}/{plotname}",
+                tol=1,
+            )
+        )
+
+    def test_output_four_curves_two_ratio(self):
+        """Test with two curves for each flavour, two ratio panels"""
+        plot = roc_plot(
+            n_ratio_panels=2,
+            ylabel="background rejection",
+            xlabel="$b$-jets efficiency",
+            atlas_second_tag=(
+                "$\\sqrt{s}=13$ TeV, PFlow Jets,\n"
+                "$t\\bar{t}$ Test Sample, $f_{c}=0.018$"
+            ),
+        )
+
+        # Add four roc curves
+        plot.add_roc(
+            roc(
+                self.sig_eff,
+                self.u_rej_1,
+                rej_class="ujets",
+                label="reference curve",
+            ),
+            reference=True,
+        )
+        plot.add_roc(
+            roc(
+                self.sig_eff,
+                self.u_rej_2,
+                rej_class="ujets",
+                label="second curve",
+            )
+        )
+        plot.add_roc(
+            roc(
+                self.sig_eff,
+                self.c_rej_1,
+                rej_class="cjets",
+                label="reference curve",
+            ),
+            reference=True,
+        )
+        plot.add_roc(
+            roc(
+                self.sig_eff,
+                self.c_rej_2,
+                rej_class="cjets",
+                label="second curve",
+            )
+        )
+
+        plot.set_ratio_class(1, "ujets", label="light-flavour jets ratio")
+        plot.set_ratio_class(2, "cjets", label="$c$-jets ratio")
+        plot.set_leg_rej_labels("ujets", "light-flavour jets rejection")
+        plot.set_leg_rej_labels("cjets", "$c$-jets rejection")
+
+        # Draw the figure
+        plot.draw()
+
+        plotname = "test_roc_four_curves_2_ratio.png"
+        plot.savefig(f"{self.actual_plots_dir}/{plotname}")
+        # Uncomment line below to update expected image
+        # plot.savefig(f"{self.expected_plots_dir}/{plotname}")
+        self.assertIsNone(
+            compare_images(
+                f"{self.actual_plots_dir}/{plotname}",
+                f"{self.expected_plots_dir}/{plotname}",
+                tol=1,
+            )
+        )
