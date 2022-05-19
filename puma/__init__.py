7db10b2652ddc4c8929ddd1871a159c0591513e0
Joschka Birk <joschka.birk@cern.ch>
2022-05-19 10:36:07 +0200

    Add build for tagged image

--- puma/__init__.py
+++ puma/__init__.py
@@ -3,7 +3,7 @@
 # flake8: noqa
 # pylint: skip-file
 
-__version__ = "0.1.0"
+__version__ = "0.1.1rc1"
 
 from puma.fraction_scan import FractionScan, FractionScanPlot
 from puma.histogram import Histogram, HistogramPlot
