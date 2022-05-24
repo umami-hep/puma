9c35080c9173c016d0670ee7e4227ddbd2909994
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 20:42:10 +0200

    add capture_output

--- docs/sphinx_build_multiversion.py
+++ docs/sphinx_build_multiversion.py
@@ -1,22 +1,20 @@
 """Script to loop over a number of git branches/tags, check them out and build the
 the sphinx docs"""
 
-from subprocess import run
+from subprocess import run, PIPE
 
 versions = [
     # "main",
     "v0.1.0",
 ]
 
-proc = run("git rev-parse --abbrev-ref HEAD", shell=True, check=True)
+proc = run("git rev-parse --abbrev-ref HEAD", shell=True, check=True, capture_output=True)
 initial_branch = proc.stdout
 
 for version in versions:
     # build the documentation
     print(f"Building docs for branch/tag {version}")
-    command = (
-        f"git checkout {version} && sphinx-build -b html source _build/html/{version}"
-    )
+    command = f"git checkout {version} "  # && sphinx-build -b html source _build/html/{version}"
     run(command, shell=True, check=True)
 
 run(f"git checkout {initial_branch}", shell=True, check=True)
