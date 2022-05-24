c882a8dd631bca582315a081a92da967d564acdf
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 16:31:49 +0200

    fix black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,7 @@ smv_branch_whitelist = r"^(main)"  # only use main branch
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 default_role = "code"
 
