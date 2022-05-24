85f36acdf768d6f9d4245fe6e955449ffcf9671d
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 22:01:31 +0200

    add sphinx_build_multiversion.py script

---
+++ docs/sphinx_build_multiversion.py
@@ -0,0 +1,32 @@
+"""Script to loop over a number of git branches/tags, check them out and build the
+the sphinx docs"""
+
+from subprocess import run
+import pygit2
+from shutil import copy, move, copytree
+import os
+import json
+
+with open("source/_static/switcher.json", "r") as f:
+    version_switcher = json.load(f)
+
+initial_branch = pygit2.Repository('.').head.shorthand
+copy("source/conf.py", "./conf_latest.py")
+
+for entry in version_switcher:
+    # build the documentation
+    print(f"Building docs for branch/tag {entry['version']}")
+    
+    # checkout the version/tag and obtain latest conf.py
+    run(f"git checkout {entry['version']}", shell=True, check=True)
+    if os.path.isfile("source/conf.py"):
+        print("removing the old conf.py file")
+        os.remove("source/conf.py")
+    copy("conf_latest.py", "source/conf.py")
+    
+    # build the docs for this version
+    run(f"sphinx-build -b html source _build/html/{entry['version']}", shell=True, check=True)
+    run("git stash", shell=True, check=True)
+
+# checkout initial branch for following steps
+run(f"git checkout {initial_branch}", shell=True, check=True)
