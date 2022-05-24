b462757d81772ffe5cf5112f56ebab25444f9a54
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 20:35:52 +0200

    black

--- docs/sphinx_build_multiversion.py
+++ docs/sphinx_build_multiversion.py
@@ -3,10 +3,20 @@ the sphinx docs"""
 
 from subprocess import run
 
-versions = ["main", "v0.1.0"]
+versions = [
+    # "main",
+    "v0.1.0",
+]
+
+proc = run("git rev-parse --abbrev-ref HEAD", shell=True, check=True)
+initial_branch = proc.stdout
 
 for version in versions:
     # build the documentation
     print(f"Building docs for branch/tag {version}")
-    command = f"git checkout {version} && sphinx-build -b html source _build/html/{version}"
+    command = (
+        f"git checkout {version} && sphinx-build -b html source _build/html/{version}"
+    )
     run(command, shell=True, check=True)
+
+run(f"git checkout {initial_branch}", shell=True, check=True)
