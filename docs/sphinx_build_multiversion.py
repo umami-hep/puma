c596586c2bee926810c713fb3782a79b94ce016b
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 22:17:11 +0200

    fix black

--- docs/sphinx_build_multiversion.py
+++ docs/sphinx_build_multiversion.py
@@ -10,25 +10,33 @@ import json
 with open("docs/source/_static/switcher.json", "r") as f:
     version_switcher = json.load(f)
 
-initial_branch = pygit2.Repository('.').head.shorthand
+initial_branch = pygit2.Repository(".").head.shorthand
 copy("docs/source/conf.py", "./conf_latest.py")
 
 for entry in version_switcher:
     # build the documentation
     print(f"Building docs for branch/tag {entry['version']}")
-    
+
     # checkout the version/tag and obtain latest conf.py
     run(f"git checkout {entry['version']}", shell=True, check=True)
     if os.path.isfile("docs/source/conf.py"):
         print("removing the old conf.py file")
         os.remove("docs/source/conf.py")
     copy("conf_latest.py", "docs/source/conf.py")
-    
-    # run librep
-    run("librep --ref_dir $PWD --input 'docs/**/*.md' --no_backup", shell=True, check=True)
-    
+
+    # run librep on markdown files (render placeholders with sytax §§§filename§§§)
+    run(
+        "librep --ref_dir $PWD --input 'docs/**/*.md' --no_backup",
+        shell=True,
+        check=True,
+    )
+
     # build the docs for this version
-    run(f"sphinx-build -b html docs/source docs/_build/html/{entry['version']}", shell=True, check=True)
+    run(
+        f"sphinx-build -b html docs/source docs/_build/html/{entry['version']}",
+        shell=True,
+        check=True,
+    )
     run("git stash", shell=True, check=True)
 
 # checkout initial branch for following steps
