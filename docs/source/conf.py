b9149fbb07f021f5829d6658c647d705f29cec88
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 16:38:48 +0200

    add remote whitelist

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,6 +60,7 @@ html_sidebars = {
 smv_tag_whitelist = r"^.*$"  # use all tagged commits
 # define which branches are used in sphinx-multiversion
 smv_branch_whitelist = r"^(main)"  # only use main branch
+smv_remote_whitelist = r"^(main)"  # only use main branch
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
