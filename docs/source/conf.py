8eff8dac3a059a9e1fb88e516e03be07aad508d9
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 17:46:10 +0200

    Check if "all branches" whitelist also just means current branch in build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,8 +59,11 @@ html_sidebars = {
 # define which tags are used in sphinx-multiversion
 smv_tag_whitelist = r"^v\d+\.\d+\.\d+$"  # only tags of the form vX.X.X
 # define which branches are used in sphinx-multiversion
-smv_branch_whitelist = r"main$"  # only use main branch
-smv_remote_whitelist = r"origin\/main$"  # only use origin/main branch
+# smv_branch_whitelist = r"main$"  # only use main branch
+smv_branch_whitelist = r'^.*$'
+# smv_remote_whitelist = r"origin\/main$"  # only use origin/main branch
+smv_remote_whitelist = None
+
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
