26c92692e9ebca15e94f6f1c714724127b94a736
Joschka Birk <joschka.birk@gmail.com>
2022-05-24 10:08:31 +0200

    Fix command

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -10,7 +10,7 @@ pip install -r docs/requirements.txt
 cd docs
 rm -rf _build _static _templates
 # sphinx-build -b html source _build/html
-sphinx-multiversion -b html source _build/html
+sphinx-multiversion source _build/html
 
 # we have to create an empty .nojekyll file in order to make the html theme work
 touch _build/html/.nojekyll
