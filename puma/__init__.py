fa6ef4c9c9aa2e88bf3d28690dbaba4a55a04036
Joschka Birk <joschka.birk@cern.ch>
2022-06-02 16:44:08 +0200

    Bump version to 0.1.2

--- puma/__init__.py
+++ puma/__init__.py
@@ -3,7 +3,7 @@
 # flake8: noqa
 # pylint: skip-file
 
-__version__ = "0.1.1dev"
+__version__ = "0.1.2"
 
 from puma.fraction_scan import FractionScan, FractionScanPlot
 from puma.histogram import Histogram, HistogramPlot
