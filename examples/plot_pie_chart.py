c479e96640c40d29e5f505ee5a50439399b36833
Joschka Birk <joschka.birk@cern.ch>
2022-06-28 15:12:46 +0200

    Use one two-line example for the pie plots

--- examples/plot_pie_chart.py
+++ examples/plot_pie_chart.py
@@ -1,13 +1,31 @@
 """Example plotting script for the puma.PiePlot class"""
 from puma.pie import PiePlot
+from puma.utils import global_config
 
 HadrTruthLabel_fracs = [70, 10, 15, 5]
 HadrTruthLabel_labels = ["Light-flavour jets", "$c$-jets", "$b$-jets", "$\\tau$-jets"]
 
-example_plot_1 = PiePlot(
+# Basic example with default values only
+example_plot_1 = PiePlot(fracs=HadrTruthLabel_fracs, labels=HadrTruthLabel_labels)
+example_plot_1.savefig("pie_example_1.png")
+
+
+# Another example with some styling
+
+# Get the flavour colours from the global config
+flav_cat = global_config["flavour_categories"]
+colours = [
+    flav_cat["ujets"]["colour"],
+    flav_cat["cjets"]["colour"],
+    flav_cat["bjets"]["colour"],
+    flav_cat["taujets"]["colour"],
+]
+
+example_plot_2 = PiePlot(
     fracs=HadrTruthLabel_fracs,
     labels=HadrTruthLabel_labels,
     draw_legend=False,
+    colours=colours,
     # have a look at the possible kwargs for matplotlib.pyplot.pie here:
     # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.pie.html
     mpl_pie_kwargs={
@@ -18,28 +36,6 @@ example_plot_1 = PiePlot(
         "radius": 1,
         "wedgeprops": dict(width=0.4, edgecolor="w"),
         "pctdistance": 0.4,
-    },
-    # kwargs passed to puma.PlotObject
-    atlas_second_tag="Dummy flavour fractions",
-    figsize=(7, 5),
-    ymax=1.5,
-)
-example_plot_1.savefig("pie_example_1.png")
-
-
-# Another example with other styling
-example_plot_2 = PiePlot(
-    fracs=HadrTruthLabel_fracs,
-    labels=HadrTruthLabel_labels,
-    draw_legend=True,
-    colour_scheme="blue",
-    # have a look at the possible kwargs for matplotlib.pyplot.pie here:
-    # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.pie.html
-    mpl_pie_kwargs={
-        "textprops": {"fontsize": 10},
-        "radius": 1,
-        "wedgeprops": dict(width=1, edgecolor="w"),
-        "pctdistance": 1.2,
     },
     # kwargs passed to puma.PlotObject
     atlas_second_tag="Dummy flavour fractions",
