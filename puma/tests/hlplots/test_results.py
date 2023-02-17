5ea9c128f30a6abecb4465cdba2eb03375443eed
Sam Van Stroud <sam.van.stroud@cern.ch>
2023-02-17 11:08:51 +0000

    fix pylint

--- puma/tests/hlplots/test_results.py
+++ puma/tests/hlplots/test_results.py
@@ -69,7 +69,7 @@ class ResultsPlotsTestCase(unittest.TestCase):
         dummy_tagger_1.label = "dummy tagger"
         self.dummy_tagger_1 = dummy_tagger_1
 
-    def assertIsFile(self, path: str):  # pylint: disable=invalid-name,no-self-use
+    def assertIsFile(self, path: str):  # pylint: disable=invalid-name
         """Check for file to exist.
         Taken from https://stackoverflow.com/a/59198749/10896585
         Parameters
