e44ae56dd9bc321887bbe797bb3794a5232b4683
Johannes Michael Wagner <johannes_wagner@berkeley.edu>
2023-10-05 02:59:08 -0700

    Changed outputs and included handling for edge cases

--- puma/utils/vertexing.py
+++ puma/utils/vertexing.py
@@ -13,8 +13,6 @@ def build_vertices(vertex_ids, ignore_indices=[-1]):
     ----------
     vertex_ids: np.ndarray
         Array containing vertex IDs for each track.
-    ignore_indices: list, optional
-        List of vertex IDs to ignore, by default [-1].
 
     Returns
     -------
@@ -32,7 +30,7 @@ def build_vertices(vertex_ids, ignore_indices=[-1]):
     vertices = np.tile(vertex_ids, (unique_ids.size, 1))
     comparison_ids = np.tile(unique_ids, (vertex_ids.size, 1)).T
     vertices = vertices == comparison_ids
-    
+
     return vertices
 
 
@@ -56,12 +54,8 @@ def associate_vertices(test_vertices, ref_vertices):
     -------
     associations: np.ndarray
         Boolean matrix of vertex associations with shape (n_test_vertices, n_ref_vertices).
-    purity: np.ndarray
-        Matrix containing vertex purity scores (fraction of tracks in test vertex also found
-        in ref vertex) for each pairing.
-    true_assoc_rate: np.ndarray
-        Matrix containing vertex true association rates (fraction of tracks in ref vertex also
-        found in true vertex) for each pairing.
+    common_tracks: np.ndarray
+        Matrix containing number of common tracks shared by each vertex pairing.
     """
     ref_sizes = ref_vertices.sum(axis=1)
     test_sizes = test_vertices.sum(axis=1)
@@ -69,27 +63,28 @@ def associate_vertices(test_vertices, ref_vertices):
     n_test = test_sizes.size
 
     common_tracks = np.dot(test_vertices.astype(int), ref_vertices.astype(int).T)
-    purity = common_tracks / np.tile(ref_sizes, (n_test, 1))
-    true_assoc_rate = common_tracks / np.tile(test_sizes, (n_ref, 1)).T
+    inv_ref_size = 1. / np.tile(ref_sizes, (n_test, 1))
+    inv_test_size = 1. / np.tile(test_sizes, (n_ref, 1)).T
 
-    pair_index = np.arange(n_ref*n_test, 0, -1).reshape(
+    pair_index = np.arange(n_ref * n_test, 0, -1).reshape(
         n_test, n_ref
-    ) # unique number for each vertex pairing
+    )  # unique number for each vertex pairing
 
     # calculate vertex associations based on maximum number of shared tracks
-    # followed by maximum purity and true association rate; if there are two
-    # equally good pairings for a vertex, the first in the list is chosen
+    # with ties broken by preferring the smallest true vertex (maximum purity score)
+    # followed by the smallest reco vertex (minimum false association); if there are
+    # two equally good pairings for a vertex, the first in the list is chosen
     associations = np.ones_like(common_tracks, dtype=bool)
-    for metric in [common_tracks, purity, true_assoc_rate, pair_index]:
+    for metric in [common_tracks, inv_ref_size, inv_test_size, pair_index]:
         metric[np.logical_not(associations)] = -1
         col_max = np.tile(np.amax(metric, axis=0), (n_test, 1))
         row_max = np.tile(np.amax(metric, axis=1), (n_ref, 1)).T
         associations = np.logical_and(metric == col_max, metric == row_max)
 
-    return associations, purity, true_assoc_rate
+    return associations, common_tracks
 
 
-def calculate_vertex_metrics(test_indices, ref_indices, max_vertices=20):
+def calculate_vertex_metrics(test_indices, ref_indices, max_vertices=20, ignore_indices=None):
     """
     Vertex metric calculator that returns a list of vertex efficiencies, fake
     rates and purity information for each jet as well as the absolute number of
@@ -104,54 +99,78 @@ def calculate_vertex_metrics(test_indices, ref_indices, max_vertices=20):
         Boolean array of shape (n_jets, n_tracks) containing vertex indices to be
         tested (reco).
     max_vertices: int, optional
-        Maximum number of vertices to write out, by default 20.
+        Maximum number of matched vertices to write out, by default 20.
+    ignore_indices: list, optional
+        List of vertex IDs to ignore, by default None.
 
     Returns
     -------
-    vertex_efficiency: np.ndarray
-        Array of shape (n_jets) containing the vertex efficiency for each jet.
-    vertex_fake_rate: np.ndarray
-        Array of shape (n_jets) containing the vertex fake rate for each jet.
-    purity: np.ndarray
-        Array of shape (n_jets, max_vertices) containing the purity score for each
-        matched vertex.
-    true_assoc_rate: np.ndarray
-        Array of shape (n_jets, max_vertices) containing the true association rate
-        for each matched vertex.
-    test_size: np.ndarray
+    n_match: np.ndarray
+        Array of shape (n_jets) containing the number of matched vertices per jet.
+    n_test: np.ndarray
+        Array of shape (n_jets) containing the number of reco vetices per jet.
+    n_ref: np.ndarray
+        Array of shape (n_jets) containing the number of truth vertices per jet.
+    track_overlap: np.ndarray
+        Array of shape (n_jets, max_vertices) containing the number of overlapping
+        tracks between each matched vertex pair.
+    test_vertex_size: np.ndarray
         Array of shape (n_jets, max_vertices) containing the number of tracks in each
-        test vertex.
-    ref_size: np.ndarray
+        matched test vertex.
+    ref_vertex_size: np.ndarray
         Array of shape (n_jets, max_vertices) containing the number of tracks in each
-        reference vertex.
+        matched reference vertex.
     """
 
-    assert ref_indices.shape == test_indices.shape, "Truth and reco vertex arrays must have the same shape."
+    assert (
+        ref_indices.shape == test_indices.shape
+    ), "Truth and reco vertex arrays must have the same shape."
     n_jets = ref_indices.shape[0]
 
-    vertex_efficiency = np.zeros(n_jets)
-    vertex_fake_rate = np.zeros(n_jets)
-    purity = np.full((n_jets, max_vertices), -1, dtype=float)
-    true_assoc_rate = np.full((n_jets, max_vertices), -1, dtype=float)
-    test_size = np.full((n_jets, max_vertices), -1)
-    ref_size = np.full((n_jets, max_vertices), -1)
+    n_match = np.zeros(n_jets, dtype=int)
+    n_test = np.zeros(n_jets, dtype=int)
+    n_ref = np.zeros(n_jets, dtype=int)
+    track_overlap = np.full((n_jets, max_vertices), -1)
+    test_vertex_size = np.full((n_jets, max_vertices), -1)
+    ref_vertex_size = np.full((n_jets, max_vertices), -1)
 
     for i in range(n_jets):
-        ref_vertices = build_vertices(ref_indices[i])
-        test_vertices = build_vertices(test_indices[i])
+        ref_vertices = build_vertices(ref_indices[i], ignore_indices=ignore_indices)
+        test_vertices = build_vertices(test_indices[i], ignore_indices=ignore_indices)
 
-        associations, purity_jet, true_assoc_rate_jet = associate_vertices(
+        # handle edge cases
+        if not ref_vertices.any() and not test_vertices.any():
+            continue
+        elif not ref_vertices.any():
+            n_test[i] = test_vertices.shape[0]
+            continue
+        elif not test_vertices.any():
+            n_ref[i] = ref_vertices.shape[0]
+            continue
+
+        associations, common_tracks = associate_vertices(
             test_vertices, ref_vertices
         )
 
-        n_match = np.sum(associations)
+        # write out vertexing efficiency metrics
+        n_match[i] = np.sum(associations)
+        n_ref[i] = ref_vertices.shape[0]
+        n_test[i] = test_vertices.shape[0]
 
-        # write out vertex metrics
-        vertex_efficiency[i] = n_match / associations.shape[1]
-        vertex_fake_rate[i] = associations.shape[0] / n_match - 1
-        purity[i, : n_match] = purity_jet[associations]
-        true_assoc_rate[i, : n_match] = true_assoc_rate_jet[associations]
-        test_size[i, : n_match] = test_vertices[associations.sum(axis=1).astype(bool)].sum(axis=1)
-        ref_size[i, : n_match] = ref_vertices[associations.sum(axis=0).astype(bool)].sum(axis=1)
+        # write out vertexing purity metrics
+        track_overlap[i, :n_match[i]] = common_tracks[associations]
+        test_vertex_size[i, :n_match[i]] = test_vertices[
+            associations.sum(axis=1).astype(bool)
+        ].sum(axis=1)
+        ref_vertex_size[i, :n_match[i]] = ref_vertices[
+            associations.sum(axis=0).astype(bool)
+        ].sum(axis=1)
 
-    return vertex_efficiency, vertex_fake_rate, purity, true_assoc_rate, test_size, ref_size
+    return (
+        n_match,
+        n_test,
+        n_ref,
+        track_overlap,
+        test_vertex_size,
+        ref_vertex_size
+    )
