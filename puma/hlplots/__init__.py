086b85d29eb88429e78a6ee35e706e785a8d6371
Nikita I Pond <zcappon@ucl.ac.uk>
2023-12-05 09:24:35 +0000

    refactored yuma into hlplots

--- puma/hlplots/__init__.py
+++ puma/hlplots/__init__.py
@@ -3,5 +3,20 @@ from __future__ import annotations
 
 from puma.hlplots.results import Results
 from puma.hlplots.tagger import Tagger
+from puma.hlplots.configs import PlotConfig
+from puma.hlplots.yutils import (
+    get_included_taggers,
+    get_plot_kwargs,
+    get_signals,
+    select_configs,
+)
 
-__all__ = ["Results", "Tagger"]
+__all__ = [
+    "Results", 
+    "Tagger",
+    "PlotConfig",
+    "get_included_taggers",
+    "get_plot_kwargs",
+    "get_signals",
+    "select_configs",
+]
