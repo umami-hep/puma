Confusion matrix: fix normalization behavior. (#262)
* Added code to calculate confusion matrix, class to plot matrix, and implemented track origin CM plot

* Fixed save path of the track origin plot, and some minor changes to comments

* Wrote a better text color dynamic choice, based on the bkg luminance, so that it works well also on diverging colormaps

* Minor changes to plot appearance

* Modified confusion_matrix so that the target labels array can contain classes that are not predicted by the classifier; fixed some comments.

* Solved the issues brought up in the MR

* Wrote unittest

* Wrote docs and examples

* Modified changelog

* Removed unused imports

* Fixed save_path in matshow example

* Modified class_names retrieval in aux_results

* Solved requested changes in the MR, except for the plot method pipeline of MatshowPlot

* Fixed unittests broke by previous commit

* Implemented public method draw for MatshowPlot class, to explicitly call the drawing on the canvas.

* Fixed normalization problem, in plotting matrix removed decimals when plotting ints

* Minor changes to comments

* Update changelog, fixed code formatting

examples/confusion_matrix_examples.py
@@ -20,14 +20,14 @@ predictions = np.random.randint(0, Nclass + 1, size=N)
 # Confusion matrix examples:
 
 # Unweighted confusion matrix, normalized on all entries
-unweighted_cm = confusion_matrix(targets, predictions)
+unweighted_cm = confusion_matrix(targets, predictions, normalize="all")
 print("Unweighted, normalized on all entries, CM:")
 print(unweighted_cm)
 print(" ")
 
 # Unweighted confusion matrix, normalized on true labels
-unweighted_cm = confusion_matrix(targets, predictions, normalize="true")
-print("Unweighted, normalized true labels, CM:")
+unweighted_cm = confusion_matrix(targets, predictions, normalize="rownorm")
+print("Unweighted, normalized true labels (rownorm), CM:")
 print(unweighted_cm)
 print(" ")
 

puma/tests/utils/test_confusion_matrix.py
@@ -14,7 +14,7 @@ class ConfusionMatrixTestCase(unittest.TestCase):
     def test_confusion_matrix_unweighted(self):
         targets = np.array([2, 0, 2, 2, 0, 1])
         predictions = np.array([0, 0, 2, 2, 0, 2])
-        cm = confusion_matrix(targets, predictions)
+        cm = confusion_matrix(targets, predictions, normalize="all")
         expected_cm = np.array([
             [0.33333333, 0.0, 0.0],
             [0.0, 0.0, 0.16666667],
@@ -22,17 +22,17 @@ class ConfusionMatrixTestCase(unittest.TestCase):
         ])
         np.testing.assert_array_almost_equal(expected_cm, cm)
 
-    def test_confusion_matrix_unweighted_normPred(self):
+    def test_confusion_matrix_unweighted_colnorm(self):
         targets = np.array([2, 0, 2, 2, 0, 1])
         predictions = np.array([0, 0, 2, 2, 0, 1])
-        cm = confusion_matrix(targets, predictions, normalize="pred")
+        cm = confusion_matrix(targets, predictions, normalize="colnorm")
         expected_cm = np.array([[0.66666667, 0.0, 0.0], [0.0, 1.0, 0.0], [0.33333333, 0.0, 1.0]])
         np.testing.assert_array_almost_equal(expected_cm, cm)
 
-    def test_confusion_matrix_unweighted_normTrue(self):
+    def test_confusion_matrix_unweighted_rownorm(self):
         targets = np.array([2, 0, 2, 2, 0, 1])
         predictions = np.array([0, 0, 2, 2, 0, 1])
-        cm = confusion_matrix(targets, predictions, normalize="true")
+        cm = confusion_matrix(targets, predictions, normalize="rownorm")
         expected_cm = np.array([
             [1.0, 0.0, 0.0],
             [0.0, 1.0, 0.0],
@@ -51,7 +51,7 @@ class ConfusionMatrixTestCase(unittest.TestCase):
         targets = np.array([2, 0, 2, 2, 0, 1])
         predictions = np.array([0, 0, 2, 2, 0, 2])
         weights = np.array([1, 0.5, 0.5, 1, 0.2, 1])
-        cm = confusion_matrix(targets, predictions, sample_weights=weights)
+        cm = confusion_matrix(targets, predictions, sample_weights=weights, normalize="all")
         expected_cm = np.array([
             [0.16666667, 0.0, 0.0],
             [0.0, 0.0, 0.23809524],

puma/utils/confusion_matrix.py
@@ -8,7 +8,7 @@ def confusion_matrix(
     targets: np.ndarray,
     predictions: np.ndarray,
     sample_weights: np.ndarray | None = None,
-    normalize: str | None = "all",
+    normalize: str | None = "rownorm",
 ) -> np.ndarray:
     """
     Parameters
@@ -22,10 +22,10 @@ def confusion_matrix(
     normalize : str | None, optional
         Normalization of the confusion matrix. Can be:
         None : Give raw counts;
-        "pred": Normalize across the prediction class, i.e. such that the rows add to one;
-        "true": Normalize across the target class, i.e. such that the columns add to one;
+        "rownorm": Normalize across the prediction class, i.e. such that the rows add to one;
+        "colnorm": Normalize across the target class, i.e. such that the columns add to one;
         "all" : Normalize across all examples, i.e. such that all matrix entries add to one.
-        Defaults to "all".
+        Defaults to "rownorm".
 
     Returns
     -------
@@ -37,9 +37,9 @@ def confusion_matrix(
     >>> predictions = np.array([0, 0, 2, 2, 0, 2])
     >>> weights = np.array([1, 0.5, 0.5, 1, 0.2, 1])
     >>> confusion_matrix(targets, predictions, sample_weights=weights)
-    np.array([[0.16666667 0.         0.        ]
-            [0.         0.         0.23809524]
-            [0.23809524 0.         0.35714286]])
+    np.array([[1.  0.  0. ]
+        [0.  0.  1. ]
+        [0.4 0.  0.6]])
     """
     # Checking that targets and predictions have the same sample size
     assert (
@@ -50,6 +50,13 @@ def confusion_matrix(
         assert (
             sample_weights.shape[0] == targets.shape[0]
         ), "confusion_matrix: Mismatch between targets' and sample weights' size"
+
+    if normalize is not None:
+        assert normalize in {
+            "rownorm",
+            "colnorm",
+            "all",
+        }, "confusion_matrix: invalid normalization keyword"
 
     # Finding number of target classes
     # (i.e. max value of the categorical indexing plus one,
@@ -68,11 +75,13 @@ def confusion_matrix(
     ).toarray()
 
     # Eventually normalize the Confusion Matrix
-    if normalize == "all":
-        cm = cm / cm.sum()
-    elif normalize == "true":
-        cm = cm / cm.sum(axis=1, keepdims=True)
-    elif normalize == "pred":
-        cm = cm / cm.sum(axis=0, keepdims=True)
+    with np.errstate(all="warn"):
+        if normalize == "all":
+            cm = cm / cm.sum()
+        elif normalize == "rownorm":
+            cm = cm / cm.sum(axis=1, keepdims=True)
+        elif normalize == "colnorm":
+            cm = cm / cm.sum(axis=0, keepdims=True)
 
-    return cm
+    # Returning the CM with nan converted to zero
+    return np.nan_to_num(cm)