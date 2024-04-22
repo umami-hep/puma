cec62e8ac292fac99c3e3a5043d679323199f9cc
Nikita I Pond <zcappon@ucl.ac.uk>
2024-04-22 09:52:46 +0100

    added check in yaml config for invalid options

--- puma/hlplots/yuma.py
+++ puma/hlplots/yuma.py
@@ -73,7 +73,28 @@ class YumaConfig:
             raise FileNotFoundError(f"Config at {path} does not exist")
         with open(path) as f:
             config = yaml.safe_load(f)
-        return cls(config_path=path, **config, **kwargs)
+        
+        config = cls(config_path=path, **config, **kwargs)
+        config._check_config()
+        return config
+        
+
+    def _check_config(self):
+        '''Checks the config for any issues, raises an error if any are found.'''
+        
+        allowed_keys = [
+            'signal', 'plot_kwargs', 
+            'include_taggers', 'exclude_taggers'
+        ]
+        for plot_type, plots in self.plots.items():
+            for p in plots:
+                for k in p.keys():
+                    if k not in allowed_keys:
+                        raise ValueError(
+                            f"Unknown key {k} in plot config. Maybe '{k}' belongs"
+                            "under 'plot_kwargs'?")
+
+        return True
 
     def get_results(self):
         """
