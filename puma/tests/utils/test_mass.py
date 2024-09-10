0a00602488bc061aff7894216e9f7c8687ac6ac9
Johannes Michael Wagner <johannes_wagner@berkeley.edu>
2024-09-10 16:47:58 -0700

    Fixed formatting

--- puma/tests/utils/test_mass.py
+++ puma/tests/utils/test_mass.py
@@ -39,9 +39,9 @@ class VertexMassTestCase(unittest.TestCase):
 
     def test_multiple_vertices(self):
         """Check more complicated case with two vertices."""
-        track_pt = np.random.rand(1, 5)*1000
-        track_eta = np.random.rand(1, 5)*2
-        track_phi = np.random.rand(1, 5)*np.pi
+        track_pt = np.random.rand(1, 5) * 1000
+        track_eta = np.random.rand(1, 5) * 2
+        track_phi = np.random.rand(1, 5) * np.pi
         vtx_idx = np.array([[0, 0, 1, 1, 1]])
 
         px = track_pt * np.cos(track_phi)
@@ -49,9 +49,21 @@ class VertexMassTestCase(unittest.TestCase):
         pz = track_pt * np.sinh(track_eta)
         e = np.sqrt(px**2 + py**2 + pz**2 + 0.13957**2)
 
-        vtx_mass_1 = np.sqrt((e[0, 0] + e[0, 1])**2 - (px[0, 0] + px[0, 1])**2 - (py[0, 0] + py[0, 1])**2 - (pz[0, 0] + pz[0, 1])**2)
-        vtx_mass_2 = np.sqrt((e[0, 2] + e[0, 3] + e[0, 4])**2 - (px[0, 2] + px[0, 3] + px[0, 4])**2 - (py[0, 2] + py[0, 3] + py[0, 4])**2 - (pz[0, 2] + pz[0, 3] + pz[0, 4])**2)
+        vtx_mass_1 = np.sqrt(
+            (e[0, 0] + e[0, 1]) ** 2
+            - (px[0, 0] + px[0, 1]) ** 2
+            - (py[0, 0] + py[0, 1]) ** 2
+            - (pz[0, 0] + pz[0, 1]) ** 2
+        )
+        vtx_mass_2 = np.sqrt(
+            (e[0, 2] + e[0, 3] + e[0, 4]) ** 2
+            - (px[0, 2] + px[0, 3] + px[0, 4]) ** 2
+            - (py[0, 2] + py[0, 3] + py[0, 4]) ** 2
+            - (pz[0, 2] + pz[0, 3] + pz[0, 4]) ** 2
+        )
 
-        vtx_masses = calculate_vertex_mass(track_pt, track_eta, track_phi, vtx_idx, particle_mass=0.13957)
+        vtx_masses = calculate_vertex_mass(
+            track_pt, track_eta, track_phi, vtx_idx, particle_mass=0.13957
+        )
         expected_result = np.array([[vtx_mass_1, vtx_mass_1, vtx_mass_2, vtx_mass_2, vtx_mass_2]])
         np.testing.assert_array_equal(vtx_masses, expected_result)
