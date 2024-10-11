1e7bde4db3d11ea3797ac711e786e01faf881f1c
Laura Caterina Vernieri <laura.pereira.sanchez@gmail.com>
2024-10-11 02:31:59 -0700

    fixing several errors of pre-commit

--- puma/utils/truth_hadron.py
+++ puma/utils/truth_hadron.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def MaskTracks(my_data, n_jets, n_tracks):
-
-    n_real_tracks = np.repeat(my_data["jets"]["n_tracks"], n_tracks).reshape(n_jets, n_tracks) # This is needed because jets have a different format than tracks
+    # This is needed because jets have a different format than tracks                                                            
+    n_real_tracks = np.repeat(my_data["jets"]["n_tracks"], n_tracks).reshape(n_jets, n_tracks)
     track_indices = np.tile(
          np.arange(0,n_tracks,dtype=np.int32),
          n_jets,
@@ -10,7 +10,7 @@ def MaskTracks(my_data, n_jets, n_tracks):
     
     track_mask = np.where(track_indices < n_real_tracks, 1, 0)
     
-    return track_mask, n_real_tracks
+    return track_mask
 
 
 def ProcessTruthHadrons(my_data, n_jets):
@@ -28,17 +28,19 @@ def ProcessTruthHadrons(my_data, n_jets):
         child.append(np.isin(hadron["ftagTruthParentBarcode"][i], hadron["barcode"][i]))
         parent.append(np.isin(hadron["barcode"][i], hadron["ftagTruthParentBarcode"][i]))
             
-    child = np.where(hadron["ftagTruthParentBarcode"] == -1, False,np.array(child))
-    parent = np.where(hadron["barcode"] == -1, False, np.array(parent))
+    child = np.where(hadron["ftagTruthParentBarcode"] is -1, False,np.array(child))
+    parent = np.where(hadron["barcode"] is -1, False, np.array(parent))
     
-    real_hadron = np.where(hadron["barcode"] == -1, False, True)
+    real_hadron = np.where(hadron["barcode"] is -1, False, True)
     one_hadron = np.where(np.sum(real_hadron, axis =1) ==1, True, False)
 
     label_hadron_index = np.where(np.sum(parent) > 0, np.argmax(parent, axis=1), np.nan)
-    label_hadron_index = np.where(one_hadron == True, 0, label_hadron_index)
+    label_hadron_index = np.where(one_hadron, 0, label_hadron_index)
 
-    child_hadron_index = np.where(np.sum(child) > 0 , np.argmax(child, axis=1), np.nan) # argmax takes the first argument from the list
-    child_hadron_index = np.where(one_hadron == True, np.nan, child_hadron_index) # make sure that if there is only one hadron, the argmax does not select 0
+    # argmax takes the first argument from the list                                                                              
+    child_hadron_index = np.where(np.sum(child) > 0 , np.argmax(child, axis=1), np.nan)
+    # make sure that if there is only one hadron, the argmax does not select 0                                                   
+    child_hadron_index = np.where(one_hadron, np.nan, child_hadron_index)
     
     child2_hadron_index = np.full(child_hadron_index.shape, np.nan)
 
@@ -55,7 +57,7 @@ def ProcessTruthHadrons(my_data, n_jets):
     print("Total number of jets = ", n_jets)
     n_one = np.sum(one_hadron)
     
-    family = np.where(hadron["barcode"] == -1, False, child | parent)
+    family = np.where(hadron["barcode"] is -1, False, child | parent)
     unrelated = ((np.sum(real_hadron, axis =1) > 1) & (np.sum(family, axis =1) <= 1))
 
     good_jets = ((one_hadron) | (np.sum(family, axis =1) > 1))
@@ -72,16 +74,16 @@ def ProcessTruthHadrons(my_data, n_jets):
         print("The sum of the 3 types of jets does not add up to the total number of jets!")
         if (np.sum(np.where(missing_jets["pdgId"] != -1, 1, 0)) == 0): 
             if (np.sum(np.where(missing_jets["flavour"] != -1, 1, 0)) == 0):
-                print("This is because for ", len(missing_jets), " non-HF jets the pdgID of all truth hadrons is -1")
+                print("bc for ", len(missing_jets), " non-HF jets pdgID=-1 for all hadrons")
 
         else:
-            print("The reason is not understood for ", np.sum(np.where(missing_jets["pdgId"] != -1, 1, 0)), " jets and you should DEBUG this")
+            print("DEBUGGING needed for ", np.sum(np.where(missing_jets["pdgId"] != -1, 1, 0)), " jets")
 
-    print("   --> # jets with 2 SVs: = ",np.sum(np.where(np.sum(family, axis =1) == 2, True, False)))
-    print("   --> # jets with 3+ SVs: = ",np.sum(np.where(np.sum(family, axis =1) > 2, True, False)))
-    print("   --> # jets with 4+ SVs: = ",np.sum(np.where(np.sum(family, axis =1) > 3, True, False)))
+    print("  --> # jets 2 SVs: = ",np.sum(np.where(np.sum(family, axis =1) == 2, True, False)))
+    print("  --> # jets 3+ SVs: = ",np.sum(np.where(np.sum(family, axis =1) > 2, True, False)))
+    print("  --> # jets 4+ SVs: = ",np.sum(np.where(np.sum(family, axis =1) > 3, True, False)))
 
-    print(np.sum(good_jets), " good jets (= the jets with more than one unrelated hadron and light jets with no truth hadrons have been dropped )")
+    print(np.sum(good_jets), " good jets (w/o unrelated hadrons)")
 
     
     return good_jets, hadron_indices, parent, child, one_hadron, unrelated
@@ -94,12 +96,12 @@ def AssociateTracksToHadron(my_data, good_jets, drop_bad_jets = True, debug=Fals
     
     # Start by getting a mask of the real tracks
     # Get real tracks
-    track_mask, n_real_tracks = MaskTracks(my_data, n_jets, n_tracks)
+    track_mask  = MaskTracks(my_data, n_jets, n_tracks)
     n_hadrons = my_data["truth_hadrons"].shape[1]
     
     if drop_bad_jets:
         my_good_jets = np.repeat(good_jets, n_tracks).reshape(n_jets, n_tracks) # This is needed because jets have a different format than tracks
-        jet_track_mask = np.where(my_good_jets == True, 1, 0)
+        jet_track_mask = np.where(my_good_jets, 1, 0)
         track_mask = track_mask & jet_track_mask
 
     inclusive_vertex = []
@@ -162,10 +164,10 @@ def AssociateTracksToHadron(my_data, good_jets, drop_bad_jets = True, debug=Fals
     return inclusive_vertex, exclusive_vertex, hadron_index, n_tracks_inclusive_vertex, n_tracks_exclusive_vertex, track_mask, jet_track_mask, good_hadron_track_association
 
 def SelectHadronMostTracks(truth_hadrons, hadron_index):
-    invalid_jet_mask = np.where(hadron_index == -99, 1, 0).astype(int)
+    invalid_jet_mask = np.where(hadron_index is -99, 1, 0).astype(int)
     
     # remove -99 so that you can get the index, but you will need to add the mask later
-    tmp_hadron_index = np.where(invalid_jet_mask == 1, 0, hadron_index)  
+    tmp_hadron_index = np.where(invalid_jet_mask, 0, hadron_index)  
 
     # select hadron with most tracks
     truth_hadron_most_tracks = truth_hadrons[np.arange(truth_hadrons.shape[0]),np.array(tmp_hadron_index).astype(int)]
