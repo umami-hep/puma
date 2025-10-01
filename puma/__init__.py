4ede3bb66d27950bf16dbcffde532d7dd9f85a24
Alexander Froch <Alexander.Froch@cern.ch>
2025-10-01 13:56:39 +0200

    Bump version

--- puma/__init__.py
+++ puma/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.4.10"
+__version__ = "0.4.11"
 
 from puma.histogram import Histogram, HistogramPlot
 from puma.integrated_eff import IntegratedEfficiency, IntegratedEfficiencyPlot
