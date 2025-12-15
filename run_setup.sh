21bfa255158e605f4ed037d37ba65c2f4e84f679
Alexander Froch <Alexander.Froch@cern.ch>
2025-12-15 14:12:25 +0100

    Adding changes to structure

--- run_setup.sh
+++
@@ -1,6 +0,0 @@
-INSTDIR=python_install
-rm -rf ${INSTDIR}
-mkdir ${INSTDIR}
-export PYTHONPATH=${PWD}:${PWD}/${INSTDIR}:${PYTHONPATH}
-python -m pip install --prefix ${INSTDIR}  -e .
-export PATH=${PWD}/${INSTDIR}/bin:$PATH
