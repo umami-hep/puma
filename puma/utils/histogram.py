30b2b3ff4ef5dc77f54bafc21e33df862af101a8
Alexander Froch <Alexander.Froch@cern.ch>
2026-09-15 11:20:08 +0200

    Update puma/utils/histogram.py
    Co-authored-by: Dmitrii Kobylianskii <kobad2@gmail.com>

--- puma/utils/histogram.py
+++ puma/utils/histogram.py
@@ -284,7 +284,6 @@ def hist_ratio(
         step_unc = np.divide(
             np.multiply(np.abs(numerator), numerator_unc),
             np.sqrt(np.abs(numerator**2 - denominator**2)),
-            out=step_unc,
             where=(numerator**2 - denominator**2 != 0),
         )
     elif method == "subtract":
